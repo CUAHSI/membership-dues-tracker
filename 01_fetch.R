80f27819eae70e90945133620580d28d05d0696c
Lindsay RC Platt <lplatt@cuahsi.org>
2024-09-27 14:55:21 -0500

    move the transaction report outside of `in/`

--- 01_fetch.R
+++ 01_fetch.R
@@ -4,7 +4,7 @@
 p1 <- list(
   
   tar_target(p1_transaction_report_csv, 
-             '01_fetch/in/transaction-report-09-23-2024.csv', 
+             'transaction-report-09-23-2024.csv', 
              format='file'),
   tar_target(p1_transaction_report, read_csv(p1_transaction_report_csv,
                                              show_col_types = FALSE))
