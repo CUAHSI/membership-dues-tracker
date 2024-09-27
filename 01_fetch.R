81c4a82c66013b1edf2b1ebb87e8217df503c370
Lindsay RC Platt <lplatt@cuahsi.org>
2024-09-27 15:01:37 -0500

    define and write targets for known membership fees

--- 01_fetch.R
+++ 01_fetch.R
@@ -3,10 +3,18 @@
 
 p1 <- list(
   
+  # Load a recent transaction report manually exported from MemberClicks
   tar_target(p1_transaction_report_csv, 
              'transaction-report-09-23-2024.csv', 
              format='file'),
   tar_target(p1_transaction_report, read_csv(p1_transaction_report_csv,
-                                             show_col_types = FALSE))
+                                             show_col_types = FALSE)),
+  
+  # Load the known membership fees data
+  tar_target(p1_membership_fees_csv, 
+             '01_fetch/in/membership_fees.csv',
+             format = 'file'),
+  tar_target(p1_membership_fees,  read_csv(p1_membership_fees_csv,
+                                           show_col_types = FALSE))
   
 )
