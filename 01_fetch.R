f4ef59c166c6ec89e11d00d70a996de3debd0562
Lindsay RC Platt <lplatt@cuahsi.org>
2024-10-22 10:57:06 -0500

    add the institution type xwalk

--- 01_fetch.R
+++ 01_fetch.R
@@ -15,7 +15,15 @@ p1 <- list(
   tar_target(p1_membership_fees_csv, 
              '01_fetch/in/membership_fees.csv',
              format = 'file'),
-  tar_target(p1_membership_fees,  read_csv(p1_membership_fees_csv,
-                                           show_col_types = FALSE))
+  tar_target(p1_membership_fees, read_csv(p1_membership_fees_csv,
+                                          show_col_types = FALSE)),
+  
+  # Load the known institution type xwalk (not checking into Git history yet)
+  tar_target(p1_institution_type_xwalk_csv, 
+             'institution_type_xwalk.csv',
+             format = 'file'),
+  tar_target(p1_institution_type_xwalk, read_csv(p1_institution_type_xwalk_csv,
+                                                 col_types = list('memberclicks_id' = 'c'),
+                                                 show_col_types = FALSE))
   
 )
