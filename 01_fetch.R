13d912d30630b6e49e132ee4dafa07704ea55a96
Lindsay RC Platt <lplatt@cuahsi.org>
2024-10-22 10:54:30 -0500

    rename `transaction_report` targets to `transaction_data`

--- 01_fetch.R
+++ 01_fetch.R
@@ -4,11 +4,12 @@
 p1 <- list(
   
   # Load a recent transaction report manually exported from MemberClicks
-  tar_target(p1_transaction_report_csv, 
+  tar_target(p1_transaction_data_csv, 
              'transaction-report-09-23-2024.csv', 
              format='file'),
-  tar_target(p1_transaction_report, read_csv(p1_transaction_report_csv,
-                                             show_col_types = FALSE)),
+  tar_target(p1_transaction_data_raw, read_csv(p1_transaction_data_csv,
+                                               col_types = list('Profile ID' = 'c'),
+                                               show_col_types = FALSE)),
   
   # Load the known membership fees data
   tar_target(p1_membership_fees_csv, 
