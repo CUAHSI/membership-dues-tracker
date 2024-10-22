79a03a016afad68690e56db31d192734ea34a44c
Lindsay RC Platt <lplatt@cuahsi.org>
2024-10-22 13:24:55 -0500

    add documentation for the formatting fxn

--- 02_clean/src/prep_transaction_data.R
+++ 02_clean/src/prep_transaction_data.R
@@ -1,5 +1,20 @@
 
-
+#' @title Format and prepare transaction columns used to identify membership status
+#' @description This function reformats and prepares columns in the raw transaction
+#' report output to prepare for determining fee types and eventually identify
+#' membership fee due dates.
+#' 
+#' @param transaction_data a tibble with the raw transaction report column from
+#' MemberClicks. Assumes the following columns are available: `Invoice ID`, 
+#' `Profile ID`, `Transaction Date`, `Amount`, and `Description`.
+#' @param institution_type_xwalk a table we put together to match institutions
+#' to an institution type. Currently matching using the MemberClicks ID provided
+#' per institution. Includes the columns `memberclicks_id`, `institution_nm`,
+#' and `institution_type`.
+#' 
+#' @returns a tibble with the columns `payment_year`, `memberclicks_id`, 
+#' `institution_nm`, `institution_type`, `invoice_id`, `date`, `amount`, and `details`.
+#' 
 format_transaction_data <- function(transaction_data, institution_type_xwalk) {
   transaction_data %>% 
     rename(invoice_id = `Invoice ID`,
